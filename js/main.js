6e3abdc1ab4cfe5808cb337f0337d68c1ff2591c
quadabra <me@quadabra.info>
2018-04-10 18:17:41 +0300

    + footer

--- js/main.js
+++ js/main.js
@@ -8,15 +8,6 @@ hamburger.addEventListener(`click`, function (evt) {
   hamburger.classList.toggle(`hamburger_active`);
 });
 
-const userMenu = document.querySelector(`.user-nav`);
-const optionsBtn = document.querySelector(`.header-navigation__options`);
-userMenu.classList.add(`menu-hidden`);
-
-optionsBtn.addEventListener(`click`, function (evt) {
-  evt.preventDefault();
-  userMenu.classList.toggle(`menu-hidden`);
-});
-
 const camoBtn = document.querySelector(`.main-nav__link_camo`);
 const camoContent = document.querySelector(`.menu-contents__section_camo`);
 
