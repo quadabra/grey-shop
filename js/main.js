2f74d76f05359e0d466eb0f0cdf5df461da1131a
quadabra <me@quadabra.info>
2018-04-18 21:54:57 +0300

    catalog help 0.1.1

--- js/main.js
+++ js/main.js
@@ -19,20 +19,22 @@ camoBtn.addEventListener(`click`, function (evt) {
 
 const helpBtn = document.querySelectorAll(`.help-block__q-button`);
 const helpWindow = document.querySelectorAll(`.help-block__answer`);
+const btnBlock = document.querySelector(`.help-block__questions`);
 
 [].forEach.call(helpWindow, (it) => it.classList.add(`help-block__answer_hidden`));
 [].forEach.call(helpBtn, (it, i) => {
   it.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     helpWindow[i].classList.toggle(`help-block__answer_hidden`);
+    btnBlock.classList.toggle(`help-block__questions_active`);
   });
 });
 
-const helpHide = document.querySelector(`.help-block__hide-btn`);
+const helpHide = document.querySelector(`.catalog-help__hide-btn`);
 const helpBlock = document.querySelector(`.help-block`);
 
 helpHide.addEventListener(`click`, (evt) => {
   evt.preventDefault();
   helpBlock.classList.toggle(`help-block_hidden`);
-  helpHide.classList.toggle(`help-block__hide-btn_closed`);
+  helpHide.classList.toggle(`catalog-help__hide-btn_closed`);
 });
